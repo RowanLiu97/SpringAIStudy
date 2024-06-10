bff16803b5830a05c071df0e8e6b260ce222dcb4
Rowan <liuhao_rowan@163.com>
2024-06-10 20:51:10 +0800

    记忆对话

--- src/main/java/com/study/controller/ChatController.java
+++ src/main/java/com/study/controller/ChatController.java
@@ -3,15 +3,22 @@ package com.study.controller;
 import com.alibaba.fastjson2.JSON;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -19,6 +26,12 @@ import reactor.core.publisher.Flux;
 public class ChatController {
     @Resource
     private OpenAiChatModel openAiChatModel;
+
+    // 历史消息列表
+    static List<Message> historyMessage = new ArrayList<>();
+
+    // 历史消息列表的最大长度
+    static int maxLen = 3;
 
 
     /**
@@ -79,6 +92,29 @@ public class ChatController {
             log.info(chatResponse.getResult().getOutput().getContent());
         });
         return stream.collectList();
+    }
+
+    @GetMapping("memoryChat")
+    public String memoryChat(@RequestParam(value = "msg") String msg){
+
+        // 用户输入的文本是UserMessage
+        historyMessage.add(new UserMessage(msg));
+        // 发给AI前对历史消息对列的长度进行检查
+        if(historyMessage.size() > maxLen){
+            log.info("历史消息列表长度超过最大长度，截取前：{}", JSON.toJSONString(historyMessage));
+            historyMessage = historyMessage.subList(historyMessage.size()-maxLen,historyMessage.size());
+            log.info("历史消息列表长度超过最大长度，截取后：{}", JSON.toJSONString(historyMessage));
+        }
+        Prompt prompt = new Prompt(historyMessage, OpenAiChatOptions.builder()
+                .withTemperature(0.6F)
+                .withModel(OpenAiApi.ChatModel.GPT_4_O)
+                .build());
+        // 获取AssistantMessage
+        ChatResponse chatResponse = openAiChatModel.call(prompt);
+        AssistantMessage assistantMessage = chatResponse.getResult().getOutput();
+        // 将AI回复的消息放到历史消息列表中
+        historyMessage.add(assistantMessage);
+        return assistantMessage.getContent();
     }
 
 }
